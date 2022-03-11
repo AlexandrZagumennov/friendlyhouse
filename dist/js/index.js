97201405926dd0f379ab39b4f70c5514aa8691e7
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-03-11 15:02:59 +0300

    МОДАЛЬНОЕ ОКНО: написан скрипт

--- dist/js/index.js
+++ dist/js/index.js
@@ -91,3 +91,56 @@ tabs.forEach((tab, index) => {
 changeContent(0);
 
 /* Табы / END */
+
+
+/* Модальное окно */
+	// Блокировка скролла
+	const disableScroll = () => {
+		const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+		document.body.dbScrollY = window.scrollY;
+
+		document.body.style.cssText = `
+			position: fixed;
+			top: ${-window.scrollY}px;
+			left: 0;
+			width: 100%;
+			height: 100vh;
+			overflow: hidden;
+			padding-right: ${widthScroll}px;
+		`
+		};
+
+		const enableScroll = () => {
+			document.body.style.cssText = '';
+			window.scroll({
+				top: document.body.dbScrollY,
+		})
+	}
+
+	// Модальное окно
+	const modalOpen = document.querySelector('.modal__open'),
+	    //  modalClose = document.querySelector('.modal__close'),
+	      modalOverlay = document.querySelector('.modal__overlay');
+
+	const runModalOpen = () => {
+		modalOverlay.classList.add('modal__overlay-open');
+		disableScroll();
+	};
+	  
+	const runModalClose = () => {
+		modalOverlay.classList.remove('modal__overlay-open');
+		enableScroll();
+	};
+	  
+	modalOpen.addEventListener('click', runModalOpen);
+	  
+	modalOverlay.addEventListener('click', event => {
+		target = event.target;
+	  
+		if (target.matches('.modal__close') || target.matches('.modal__overlay')) {
+			runModalClose();
+		 }
+	});
+
+/* Модальное окно / END  */
